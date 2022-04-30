04a8363d8e3612cbed1fcdcf0567d3f5b9c0273a
Anastatia Dorval <anastatiad013@gmail.com>
2022-04-30 14:23:44 -0500

    connect landing page to backend

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import MeasurementForm from './MeasurementForm'
 
 class LoginForm extends Component {
-    constructor(props) {
+    constructor(props){
         super(props)
         this.state = {
+            status: '',
             username: '',
-            password: ''
+            password: '',
+            measurements: {},
         }
     }
 
@@ -16,9 +20,45 @@ class LoginForm extends Component {
     }
 
     handleSubmit = event => {
-        alert(` Username: ${this.state.username} 
-                Password: ${this.state.password}`)
-        event.preventDefault()
+        event.preventDefault()      
+        axios.post(`https://anastatiad.pythonanywhere.com/signupLP`, 
+            { 
+                'username': this.state.username, 
+                'password': this.state.password, 
+                'measurements' : this.state.measurements 
+            }
+        )
+        .then(
+            (response) => {
+                var result = response.data;
+                this.state.status = result.status;
+                console.log(result);
+                if (this.state.status == 'User does not exist') {
+                    //prompt user to sign up
+                    //switch to sign up pop up (how do I do that?)
+                }
+                else {
+                    //go to next page (measurements)
+                    //switch to measurements pop up
+                }
+            },
+            (error) => {
+                console.log(error);
+            }
+        );
+        axios.get('https://anastatiad.pythonanywhere.com/signupLP')
+        .then((response) => {
+            const res =response.data
+            this.state.status = res.status
+            
+        })
+        .catch((error) => {
+            if (error.response) {
+              console.log(error.response)
+              console.log(error.response.status)
+              console.log(error.response.headers)
+            }
+        });
     }
 
 
@@ -28,7 +68,7 @@ class LoginForm extends Component {
             <label>Username:
                 <input type='text'
                 name='username'
-                value={this.state.username}
+                value={this.state.Username}
                 onChange={this.handleInput}
                 />
             </label>
@@ -36,12 +76,12 @@ class LoginForm extends Component {
             <label>Password:
                 <input type='text' 
                 name='password'
-                value={this.state.password}
+                value={this.state.Password}
                 onChange={this.handleInput}
                 />
             </label>  
 
-            <button type="submit">Enter</button>  
+            <button type="submit">Enter</button>
         </form>
     )
   }
