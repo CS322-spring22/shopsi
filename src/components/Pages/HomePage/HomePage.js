38700625d1d4ee2d823b75d75675cdca61671028
Tavian James <tavianjames12@gmail.com>
2022-05-03 16:08:36 -0500

    func to class

--- src/components/Pages/HomePage/HomePage.js
+++ src/components/Pages/HomePage/HomePage.js
@@ -1,48 +1,52 @@
-import React from 'react'
-import './HomePage.css';
-import Navbar from '../../NavBar/NavBar';
+import React from "react";
+import "./HomePage.css";
+import Navbar from "../../NavBar/NavBar";
+import { Component } from "react";
 
-const HomePage = () => {
-  return (
-    <div className="homepage"> 
-      <Navbar/>  
-      <div className="header">
-        <div className="header-title"> 
-          <h2>Shop$i</h2>
+export class HomePage extends Component {
+  render() {
+    return (
+      <div className="homepage">
+        <Navbar />
+        <div className="header">
+          <div className="header-title">
+            <h2>Shop$i</h2>
+          </div>
+          <div className="header-body">
+            <h1>
+              Your clothes. <br />
+              Your style. <br />
+              Your fit.
+            </h1>
+            <a href="https://chrome.google.com/webstore/category/extensions?hl=en">
+              <span className="install-extension-link">Install Extension</span>
+            </a>
+          </div>
         </div>
-        <div className="header-body">
-          <h1>Your clothes. <br/>Your style. <br/>Your fit.</h1> 
-          <a href="https://chrome.google.com/webstore/category/extensions?hl=en">
-            <span className= "install-extension-link">
-              Install Extension 
-            </span>
-          </a>
-        </div>  
+
+        <div className="about" id="about">
+          <div className="content-header">
+            <h2>About Shop$i</h2>
+          </div>
+
+          <div className="about-body">
+            <p>
+              No more returns. Shopsi automatically selects the size you want
+              based on your preferences. Online shopping has never been easier!
+            </p>
+          </div>
+        </div>
+
+        <div className="how-to" id="how-to">
+          <div className="content-header">
+            <h2>How to use Shop$i</h2>
+          </div>
+          <div className="measurements"></div>
+        </div>
+
+        <div className="footer"></div>
       </div>
-
-      <div className="about" id="about">
-        <div className="content-header">
-          <h2>About Shop$i</h2>
-        </div>
-        
-        <div className="about-body"> 
-        <p>No more returns. Shopsi automatically selects the size you want based on your preferences. 
-          Online shopping has never been easier!</p>
-        </div>
-      </div>
-
-      <div className="how-to" id="how-to">
-        <div className='content-header'>
-          <h2>How to use Shop$i</h2>
-        </div>
-        <div className="measurements">
-
-        </div>
-      </div>
-
-      <div className="footer">
-      </div>
-    </div>
-  )
+    )
+  }
 }
-export default HomePage
+export default HomePage;
--- src/components/Pages/MeasurementPage/MeasurementPage.js
+++ src/components/Pages/MeasurementPage/MeasurementPage.js
@@ -1,70 +1,71 @@
-import React from 'react'
-import Slider from '../../Slider/Slider'
+import React, { Component } from 'react'
+
 import './MeasurementPage.css'
 
+ export class MeasurementPage extends Component {
+    render() {
+        return (
+            <div className='MeasurementPage'>
+            <h1>Shop$i</h1>
+            <h3 className="desc">Shop with your size</h3><br></br>
+            <div class="measurement">
+                <form>
+                    <h2>Enter your measurements</h2>
+                    <div class="measurements">
+                        <div class="box">
+                            <label class="mesName">Bust/Chest</label>
+                            <div class="sliderBar">
+                                <input type="range"  min="0" max ="100"/>
+                                <div class ="value" id="bust-value">0</div>
+                            </div>
+                        </div>
+                        
+                        <div class="box">
+                            <label class="mesName">Neckline</label>
+                            <div class="sliderBar">
+                                <input type="range" id="neckSlider" min="0" max ="100"/>
+                                <div class ="value" id="neck-value">0</div>
+                            </div>
+                        </div>
 
-export default function MeasurementPage() {
+                        <div class="box">
+                            <label class="mesName">Waist</label>
+                            <div class="sliderBar">
+                                <input type="range" id="waistSlider" min="0" max ="100"/>
+                                <div class ="value" id="waist-value">0</div>
+                            </div>
+                        </div>
 
-  return (
-    <div className='MeasurementPage'>
-      <h1>Shop$i</h1>
-      <h3 className="desc">Shop with your size</h3><br></br>
-      <div class="measurement">
-        <form>
-            <button id="log-out">Log out</button>
-            <h2>Enter your measurements</h2>
-            <div class="measurements">
-                <div class="box">
-                    <label class="mesName">Bust/Chest</label>
-                    <div class="sliderBar">
-                        <input type="range"  min="0" max ="100" value ="0"/>
-                        <div class ="value" id="bust-value">0</div>
+                        <div class="box">
+                            <label class="mesName">Low Hip</label>
+                            <div class="sliderBar">
+                                <input type="range" id="hipSlider" min="0" max ="100"/>
+                                <div class ="value" id="hip-value">0</div>
+                            </div>
+                        </div>
+
+                        <div class="box">
+                            <label class="mesName">Arm length</label>
+                            <div class="sliderBar">
+                                <input type="range" id="armSlider" min="0" max ="100"/>
+                                <div class ="value" id="arm-value">0</div>
+                            </div>
+                        </div>
+
+                        <div class="box">
+                            <label class="mesName">Inside leg</label>
+                            <div class="sliderBar">
+                                <input type="range" id="legSlider" min="0" max ="100"/>
+                                <div class ="value" id="leg-value">0</div>
+                            </div>      
+                        </div> 
                     </div>
-                </div>
-                
-                <div class="box">
-                    <label class="mesName">Neckline</label>
-                    <div class="sliderBar">
-                        <input type="range" id="neckSlider" min="0" max ="100" value ="0"/>
-                        <div class ="value" id="neck-value">0</div>
-                    </div>
-                </div>
-
-                <div class="box">
-                    <label class="mesName">Waist</label>
-                    <div class="sliderBar">
-                        <input type="range" id="waistSlider" min="0" max ="100" value ="0"/>
-                        <div class ="value" id="waist-value">0</div>
-                    </div>
-                </div>
-
-                <div class="box">
-                    <label class="mesName">Low Hip</label>
-                    <div class="sliderBar">
-                        <input type="range" id="hipSlider" min="0" max ="100" value ="0"/>
-                        <div class ="value" id="hip-value">0</div>
-                    </div>
-                </div>
-
-                <div class="box">
-                    <label class="mesName">Arm length</label>
-                    <div class="sliderBar">
-                        <input type="range" id="armSlider" min="0" max ="100" value ="0"/>
-                        <div class ="value" id="arm-value">0</div>
-                    </div>
-                </div>
-
-                <div class="box">
-                    <label class="mesName">Inside leg</label>
-                    <div class="sliderBar">
-                        <input type="range" id="legSlider" min="0" max ="100" value ="0"/>
-                        <div class ="value" id="leg-value">0</div>
-                    </div>      
-                </div> 
+                    <button type="submit" id="enter-measurement" >Submit</button>
+                </form>
             </div>
-            <button type="submit" id="enter-measurement" >Submit</button>
-        </form>
-      </div>
-    </div>
-  )
+            </div>
+        )
+    }
 }
+
+export default MeasurementPage
