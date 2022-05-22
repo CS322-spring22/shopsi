6736f6ce87adc24c038fa11accda3ff2ffac0ee5
Tavian James <tavianjames12@gmail.com>
2022-05-22 17:25:54 -0500

    Added unit switch functionality

--- src/components/Switch/Switch.js
+++ src/components/Switch/Switch.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "./Switch.css";
 import { useState } from "react";
-
+ 
 function Switch() {
-  const [isToggled, setIsToggled] = useState(false);
-  const onToggle = () => setIsToggled(!isToggled);
-  return (
-    <>
-      <label className="toggle-switch">
-        <input type="checkbox" checked={isToggled} onChange={onToggle} />
-        <span className="switch" />
-      </label>
-    </>
-  );
+ const [isToggled, setIsToggled] = useState(false);
+ const onToggle = () => {setIsToggled(!isToggled)}
+ return (
+   <>
+     <label className="toggle-switch">
+       <input type="checkbox" checked={isToggled} onChange={onToggle} />
+       <span className="switch" />
+     </label>
+   </>
+ );
 }
-
+ 
 export default Switch;
